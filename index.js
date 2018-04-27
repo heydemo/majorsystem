034777ca2e015092499926b562863d11e2c8837e
John De Mott <john@fourkitchens.com>
2018-04-27 18:37:47 -0500

    refactoring

--- index.js
+++ index.js
@@ -1,162 +1,57 @@
-var prettyjson = require('prettyjson');
-var map = require('./map.json');
-var fs = require('fs');
+#!/usr/bin/env node
 
-function numberToLetters(number, start = '') {
-  var numb = String(number);
-  if (numb.length == 0) {
-    return [start];
-  }
-  var firstDigit = numb.slice(0, 1);
-  var remainder = numb.slice(1);
-  var choices = [];
-  for (letter of map[firstDigit]) {
-    choices = choices.concat(numberToLetters(remainder, start + letter));
-  }
-  return choices;
+const ms = require('./majorsystem.js');
+
+var argv = require('yargs')
+  .usage('majorsystem [number]')
+  .demandCommand(1)
+  .option('depth', {
+    alias: 'd',
+    describe: 'Maximum number of words for the mnemonic',
+    default: 3,
+  })
+  .option('limit', {
+    describe: 'Maximum number of mnemonics to display',
+    default: 10,
+  })
+  .option('colorize', {
+    describe: 'Colorize the consonants',
+    count: true,
+  })
+  .option('capitalize', {
+    describe: 'Colorize the consonants',
+    count: true,
+  })
+  .help('help')
+  .alias('help', 'h')
+  .argv;
+
+var num = argv._[0];
+var limit = argv.limit;
+
+var options = {
+  maxDepth: argv.depth,
 }
 
-function startsWith(start) {
-  return function (word) {
-    return (word.indexOf(start) === 0);
-  }
-}
 
-function removeVowels(str) {
-  return str.replace(/[aeiou]/gi, '');
-}
+const start = new Date().getTime();
 
-function getWords(callback) {
-  fs.readFile('wordsEn.txt', function(err, data) {
-      if (err) throw err;
-      callback(data.toString().split("\n"));
-  });
-}
-
-function getDevoweledWords(words) {
-  var transformations = {};
-  for (word of words) {
-    var wordWithoutVowels = removeVowels(word);
-    if (!transformations[wordWithoutVowels]) {
-      transformations[wordWithoutVowels] = [];
+ms.getMnemonics(num, options)
+  .then((mnems) => {
+    const time = new Date().getTime() - start;
+    if (limit) {
+      mnems = mnems.slice(0, limit);
     }
-    transformations[wordWithoutVowels].push(word);
-  }
-  return transformations;
-}
-
-function wordArrayToStrings(wordArray, line = []) {
-  if (!wordArray.length) {
-    console.log('line:');
-    console.log(line);
-    return;
-  }
-  var words = wordArray.shift();
-  for (word of words) {
-    wordArrayToStrings(wordArray, line.concat(word));
-  }
-}
-
-function flattenWords(wordArray, line = '', lines = []) {
-  console.log(arguments);
-  var lines = [];
-  var words = wordArray[0];
-  for (word of words) {
-    console.log('word: ' + word);
-    if (wordArray.length == 1) {
-      lines.push(line + word);
+    
+    if (argv.capitalize) {
+      mnems = mnems.map(ms.capitalizeConsonants);
     }
-    else {
-      flattenWords(wordArray.slice(1), line + ' ' + word, lines);
-    }
-  }
-  return lines;
-}
-
-function flattenLine(line) {
-  if (typeof line[0] == 'string') {
-    return line;
-  }
-  var words = wordArray[0];
-
-
-
-}
-
-//var choices = numberToLetters(1917);
-function getMnems(devowledWord, words, depth = 0) {
-  var matches = [];
-  for (var i = devowledWord.length; i > 0; i--) {
-    var word = devowledWord.substring(0, i)
-    if (words[word]) {
-      var remainder = devowledWord.substring(i);
-      if (!remainder) {
-        matches.push({ depth, words: words[word] });
-      }
-      else if (matches < 100000 && depth < 3) {
-        matches.push({ depth, words: words[word], children: getMnems(remainder, words, depth + 1) });
-      }
-    }
-  }
-  return matches;
-}
-
-function transLine(line, start = '') { // return String[] i.e. ['fax ado', 'fax ad', 'fax adieu', 'fix ado']
-  var mnems = [];
-  for (word of line.words) {
-    var next = start ? start +' '+ word : word;
-    if (line.children) {
-      for (child of line.children) {
-        mnems = mnems.concat(transLine(child, next));
-      }
-    }
-    else {
-      mnems.push(next);
-    }
-  }
-  return mnems;
-}
-
-function transLines(lines) {
-  return [].concat.apply([], lines.map(line => transLine(line)));
-}
-
-function cap(str){
-   return str.toUpperCase().replace(/[AEIOU]/g, function(l) {
-         return l.toLowerCase();
-           })
-}
-
-
-//console.log(choices);
-getWords(function (words) {
-  //var numb = 1917;
-  //var wordMap = getDevoweledWords(words);
-  //console.log(Object.keys(wordMap).map(removeVowels).filter(startsWith('fxd')));
-  /**
-  var mnems = getMnems('fxd', words);
-  //var lines = flattenWords(mnems);
-  //console.log(lines);
-  console.log(prettyjson.render(mnems));
-  console.log(JSON.stringify(mnems));
-  trans = transLines(mnems);
-  console.log(trans);
-  */
-  words = getDevoweledWords(words);
-
-  var num = process.argv[2];
-  var out = numberToLetters(num)
-    .map(function (letters) { 
-      return getMnems(letters, words)
-    })
-    .map((mnems) => transLines(mnems))
-      
-  out = [].concat.apply([], out).map(cap);
-
-  out.forEach(line => console.log(line));
-
-
-});
+    
+    mnems.map((mnem) => {
+      ms.print(mnem, argv.colorize);
+    });
+  })
+  .catch(console.log)
 
 
 
---
+++ majorsystem.js
@@ -0,0 +1,180 @@
+var prettyjson = require('prettyjson');
+var map = require('./map.json');
+var fs = require('fs');
+const { promisify } = require('util');
+const readFileAsync = promisify(fs.readFile);
+const chalk = require('chalk');
+
+function numberToLetters(number, start = '') {
+  var numb = String(number);
+  if (numb.length == 0) {
+    return [start];
+  }
+  var firstDigit = numb.slice(0, 1);
+  var remainder = numb.slice(1);
+  var choices = [];
+  for (letter of map[firstDigit]) {
+    choices = choices.concat(numberToLetters(remainder, start + letter));
+  }
+  return choices;
+}
+
+function removeVowels(str) {
+  return str.replace(/[aeiou]/gi, '');
+}
+
+function getWords() {
+  return readFileAsync('wordsEn.txt')
+    .then((data) => {
+      return data.toString().split("\n");
+    })
+    .catch((error) => console.log(error));
+ }
+
+function getDevoweledWords(words) {
+  var transformations = {};
+  for (word of words) {
+    var wordWithoutVowels = removeVowels(word);
+    if (!transformations[wordWithoutVowels]) {
+      transformations[wordWithoutVowels] = [];
+    }
+    transformations[wordWithoutVowels].push(word);
+  }
+  return transformations;
+}
+
+function wordArrayToStrings(wordArray, line = []) {
+  if (!wordArray.length) {
+    console.log('line:');
+    console.log(line);
+    return;
+  }
+  var words = wordArray.shift();
+  for (word of words) {
+    wordArrayToStrings(wordArray, line.concat(word));
+  }
+}
+
+
+/**
+ * Returns array of mnemonics objects from devowled word.
+ *
+ * @param {options} 
+ *   options.words - array of word arrays, keyed by devowled word
+ *   options.maxDepth - Maximum number of separate words.
+ */
+function getMnems(devowledWord, options = {}, depth = 0)  { // return Object[]
+  var words = options.words;
+  var matches = [];
+  for (var i = devowledWord.length; i > 0; i--) {
+    var word = devowledWord.substring(0, i)
+    if (words[word]) {
+      var remainder = devowledWord.substring(i);
+      if (!remainder) {
+        matches.push({ depth, words: words[word] });
+      }
+      else if (depth < options.maxDepth) {
+        matches.push({ depth, words: words[word], children: getMnems(remainder, options, depth + 1) });
+      }
+    }
+  }
+  return matches;
+}
+
+function capitalizeConsonants(str){
+  return str.toUpperCase().replace(/[AEIOU]/g, function(l) {
+   return l.toLowerCase();
+  });
+}
+
+function sortByWordCount(a, b) {
+  return a.split(' ').length - b.split(' ').length
+}
+ 
+function arrayReducer(flattenedArray, item) {
+  return flattenedArray.concat(item); 
+}
+
+function transLine(line, start = '') { // return String[] i.e. ['fax ado', 'fax ad', 'fax adieu', 'fix ado']
+  var mnems = [];
+  for (word of line.words) {
+    var next = start ? start +' '+ word : word;
+    if (line.children && line.children.length) {
+      for (child of line.children) {
+        mnems = mnems.concat(transLine(child, next));
+      }
+    }
+    else {
+      mnems.push(next);
+    }
+  }
+  return mnems;
+}
+
+function transLines(lines) {
+  return [].concat.apply([], lines.map(line => transLine(line)));
+}
+
+function cap(str){
+   return str.toUpperCase().replace(/[AEIOU]/g, function(l) {
+         return l.toLowerCase();
+           })
+}
+
+function combineArrays(arrs) {
+  return [].concat.apply([], arrs);
+}
+
+
+function getMnemonics(num, options) {
+  return getWords()
+    .then((words) => {
+      words = getDevoweledWords(words);
+      options.words = words;
+
+      var letters = numberToLetters(num);
+      var mnems   = combineArrays(letters.map((letters) => getMnems(letters, options)));
+      var lines = transLines(mnems);
+      console.log('got them lines');
+
+      return lines;
+
+
+      //var out = mnems.map((mnem) => transLines(mnem));
+      //return combineArrays(out);
+        /*
+        .map(function (letters) { 
+          return getMnems(letters, options)
+        }))
+        .map((mnems) => transLines(mnems))
+          
+      return [].concat.apply([], out);
+      */
+
+    })
+    .catch((error) => console.log(error));
+}
+
+function print(word, colorize) {
+  if (!colorize) {
+    console.log(word);
+    return;
+  }
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  var coloredWord = '';
+  for (letter of word) {
+    if (!vowels.includes(letter)) {
+      coloredWord += chalk.blue(letter);
+    }
+    else {
+      coloredWord += letter;
+    }
+  }
+  console.log(coloredWord);
+}
+
+module.exports = {
+  getMnemonics,
+  print,
+  capitalizeConsonants
+}
